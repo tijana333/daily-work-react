e7f84a609915bf4f80cead534572ff92610f84ba
tijana333 <150601653+tijana333@users.noreply.github.com>
2026-04-23 13:15:38 +0200

    DWRJ-6

--- src/components/EntryCard/EntryCard.jsx
+++ src/components/EntryCard/EntryCard.jsx
@@ -2,8 +2,13 @@ function EntryCard({ entry }) {
   return (
     <div className="entry-card">
       <div className="entry-date">{entry.date}</div>
-      <div className="entry-hours">{entry.hours}</div>
-      <div className="entry-challenge">{entry.challenge}</div>
+
+      <div className="entry-content">
+        <div className="hours">{entry.hours}h</div>
+        <div className="challenge">{entry.challenge}</div>
+      </div>
+
+      <div className="entry-intensity">{entry.intensity}</div>
     </div>
   );
 }
