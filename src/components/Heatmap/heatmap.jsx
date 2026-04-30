4a8c88538c767c7c4061fd08baff6aea3b814676
tijana333 <150601653+tijana333@users.noreply.github.com>
2026-04-30 10:20:52 +0200

    heatmap

--- src/components/Heatmap/heatmap.jsx
+++ src/components/Heatmap/heatmap.jsx
@@ -1,5 +1,40 @@
 function Heatmap() {
-  return <div>Heatmap</div>;
+  const today = new Date();
+
+  const currentYear = today.getFullYear();
+  const currentMonth = today.getMonth();
+
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+  const monthName = today.toLocaleString("en-US", {
+    month: "long",
+  });
+
+  const days = Array.from({ length: daysInMonth }, (_, index) => index + 1);
+
+  return (
+    <section className="heatmap-section">
+      <div className="carousel">
+        <button type="button">‹</button>
+
+        <h2 id="current-month">
+          {monthName} {currentYear}
+        </h2>
+
+        <button type="button">›</button>
+      </div>
+
+      <div className="heatmap-container">
+        <div className="heatmap-box has-data">
+          <div className="heatmap-grid">
+            {days.map((day) => (
+              <div key={day} className="heatmap-day"></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Heatmap;
